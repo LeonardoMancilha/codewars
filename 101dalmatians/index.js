5bbde6a6aad50695d2409110579d133cc4f91c51
Leonardo Machado <leonardomachado.sjc@gmail.com>
2024-11-26 17:46:16 -0300

    Corrige a lógica da função para determinar a resposta com base na quantidade de cães fornecida.

---
+++ 101dalmatians/index.js
@@ -0,0 +1,23 @@
+function howManyDalmatians(number) {
+  let dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIANS!!!",
+  ];
+
+  let respond =
+    number <= 10
+      ? dogs[0]
+      : number <= 50
+      ? dogs[1]
+      : number <= 100
+      ? dogs[2]
+      : (number = 101)
+      ? dogs[3]
+      : "";
+
+  return respond;
+}
+
+console.log(howManyDalmatians(101));
