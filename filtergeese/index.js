0abdce92edb1339c65deac55e7cf9674fdfd5aa1
Leonardo Machado <leonardomachado.sjc@gmail.com>
2025-01-09 19:35:24 -0300

    Implementar função para remover gansos de uma lista de aves

---
+++ filtergeese/index.js
@@ -0,0 +1,17 @@
+function gooseFilter(birds) {
+  const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  return birds.filter((bird) => !geese.includes(bird));
+}
+
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+);
