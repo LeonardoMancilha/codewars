b6931720fb887973e4a08163c37ce0058ae2c3f5
Leonardo Machado <leonardomachado.sjc@gmail.com>
2025-01-31 22:40:31 -0300

    implementar função para retornar múltiplos de um número até o limite especificado.

---
+++ findmultipleofanumber/index.js
@@ -0,0 +1,11 @@
+function findMultiples(integer, limit) {
+    const multiples = [];
+    
+    for (let i = integer; i <= limit; i += integer) {
+        multiples.push(i);
+    }
+    
+    return multiples;
+}
+
+console.log(findMultiples(5, 25)); // [5, 10, 15, 20]
